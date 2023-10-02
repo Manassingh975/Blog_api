c0c41fdc0bdde335074c2bacca4ea05c80506aa3
Manas Kumar <manas25112@gmail.com>
2023-10-03 01:11:00 +0530

    mongodb-connection

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-
-mongoose.connect("mongodb://127.0.0.1:27017/blog_api", { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-}).then( () => {
+// realmanassingh   FvFQgRFZ5GrZrKH5   203.192.204.112/32
+const MONGODB_CONNECT_URI = "mongodb+srv://realmanassingh:FvFQgRFZ5GrZrKH5@cluster0.kotlqml.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(process.env.MONGODB_CONNECT_URI,)
+.then( () => {
     console.log("Connection successfull");
 }).catch((e) => {
     console.log("No Connection", e);
